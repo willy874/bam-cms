77acfa6ac07681b0fd857d5d441cc524a60a0b38
willy <willy8742891@gmail.com>
2023-04-23 17:13:14 +0800

    feat: keep

--- packages/database/src/main.ts
+++ packages/database/src/main.ts
@@ -2,22 +2,22 @@ import { DATABASE_HOST, DATABASE_NAME, DATABASE_PASSWORD, DATABASE_PORT, DATABAS
 import { MysqlOperator, Migration, DataType } from '@/core';
 
 (async function () {
-  class TestMigration extends Migration {
-    async up() {
-      await this.create('test', (table) => {
-        table.schema({
-          id: DataType.integer().autoIncrementing().primary(),
-          name: DataType.string(),
-          age: DataType.integer(),
-          created_at: DataType.date().autoDefault(),
-          updated_at: DataType.date().autoDefault(),
-        });
-      });
-    }
-    async down() {
-      await this.drop('test');
-    }
-  }
+  // class TestMigration extends Migration {
+  //   async up() {
+  //     await this.create('test', (table) => {
+  //       table.schema({
+  //         id: DataType.integer().autoIncrementing().primary(),
+  //         name: DataType.string(),
+  //         age: DataType.integer(),
+  //         created_at: DataType.date().autoDefault(),
+  //         updated_at: DataType.date().autoDefault(),
+  //       });
+  //     });
+  //   }
+  //   async down() {
+  //     await this.drop('test');
+  //   }
+  // }
   const operator = new MysqlOperator();
   operator.on('connect', () => console.log('connect'));
   await operator.connect({
